75f5c889f06d866a69875d97d743da95497fdd59
Rune Mydske Nielsen <runeni@rapido.(none)>
2013-03-12 23:16:53 +0100

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brygg::Application.config.secret_token = '12d5e2c28855027b274fa72ca107b6e4e07f654a0223263ca632c82db037895e72c7b69d684bdc87ef5cd68e9195b6ff8523fb48ea69bfe5a874ee10cb2c2cbd'
