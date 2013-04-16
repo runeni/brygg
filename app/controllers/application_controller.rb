20615d5ae93cde8fdc115b134f57b39a8d181cad
runeni <rune@template.no>
2013-04-16 22:09:03 +0200

    Store chosen locale in session.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
    
   def set_locale
-      I18n.locale = params[:locale] || I18n.default_locale
+    if params[:locale]
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = params[:locale] || session[:locale] || I18n.default_locale
   end
 end
