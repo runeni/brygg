c267785886c252ad8e5d5a26a31bf380d2bdb9fb
runeni <rune@template.no>
2013-04-16 21:18:05 +0200

    Point root_url to /brews.

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ Brygg::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'application#index'
+  root :to => 'brews#index'
 
   # See how all your routes lay out with "rake routes"
 
